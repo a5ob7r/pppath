9a6f95266312371c06e112692207cb494ddf22d8
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2022-04-29 19:14:04 +0900

    Initialize

---
+++ pppath.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+pppath () {
+  local -r version=0.1.0
+
+  local -a args=()
+  local color=auto
+  local format=pretty
+
+  while (( $# )); do
+    case "$1" in
+      --version )
+        echo "$version"
+        return 0
+        ;;
+      --help | -h )
+        echo -n "\
+Descriptions:
+  Pretty printer for \$PATH. This substitutes each path prefixes with a literal
+  tilda(~) if the prefix is \$HOME's value, the substitution is inspired by
+  zsh's \"print -D\". And this decorates each paths with some colors, but skips
+  decorations for an empty path.
+
+  1. If no directory exists at the path, Reverse Red
+  2. If the path is duplicated, Red
+  3. If the path is well known or in general or contained in defaults, Green
+  4. If the path prefix is \$HOME, None
+  5. Otherwise, Yellow
+
+Usages:
+  pppath [--color=WHEN] [--format=FORMAT] [PATH...]
+
+Globals:
+  NO_COLOR: Disable text decorations with SGR forcibly.
+
+Arguments:
+  PATH  Optional multiple, and probably directpry paths. This is \$PATH if
+        omitted.
+
+Options:
+  --color=WHEN     Whether or not coloring output. Valid values of WHEN are
+                   'always', 'never', and 'auto'. The default value is 'auto'.
+  --format=FORMAT  Whether or not using pretty print format. Valid values of
+                   FORMAT are 'raw' and 'pretty'. The default value is 'pretty'.
+  --help, -h       Show this message and exit
+  --version        Show the version info and exit
+"
+        return 0
+        ;;
+      --color=always )
+        color=always
+        shift
+        ;;
+      --color=never )
+        color=never
+        shift
+        ;;
+      --color=auto )
+        color=auto
+        shift
+        ;;
+      --format=raw )
+        format=raw
+        shift
+        ;;
+      --format=pretty )
+        format=pretty
+        shift
+        ;;
+      -- )
+        shift
+        args+=("$@")
+        shift $#
+        ;;
+      -* )
+        echo "[path] An unknown option: $1."
+        return 1
+        ;;
+      * )
+        args+=("$1")
+        shift
+        ;;
+    esac
+  done
+
+  if (( ${#args[@]} == 0 )); then
+    args=("$PATH")
+  fi
+
+  # Assume almost all terminals can interpret SGR.
+  if [[ $color == always || $color != never && $color == auto && -z ${NO_COLOR+1} && -t 1 && $TERM != dumb ]]; then
+    red='\033[31m'
+    reversered='\033[7;31m'
+    green='\033[32m'
+    yellow='\033[33m'
+    reset='\033[0m'
+  else
+    red=''
+    reversered=''
+    green=''
+    yellow=''
+    reset=''
+  fi
+
+  # A common value of $PATH.
+  local -r wellknowns=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
+
+  local -r path="$(IFS=:; echo "${args[*]}")"
+
+  # Each path components.
+  local -a paths=()
+  IFS=: read -r -a paths <<<"$path"
+
+  for p in "${paths[@]}"; do
+    # Substitute $HOME with a literal tilda(~) if a prefix of the path is
+    # $HOME.
+    if [[ $format == pretty ]]; then
+      case $p in
+        ~ )
+          local label='~'
+          ;;
+        ~/* )
+          local label="~${p:${#HOME}}"
+          ;;
+        * )
+          local label=$p
+          ;;
+      esac
+    else
+      local label=$p
+    fi
+
+    if [[ -z $p ]]; then
+      echo
+    elif [[ ! -d $p ]]; then
+      echo -e "$reversered$label$reset"
+    elif [[ :$path: == *:$p:$p:* || :$path: == *:$p:*:$p:* ]]; then
+      echo -e "$red$label$reset"
+    elif [[ :$wellknowns: == *:$p:* ]]; then
+      echo -e "$green$label$reset"
+    elif [[ $p == ~ || $p == ~/* ]]; then
+      echo "$label"
+    else
+      echo -e "$yellow$label$reset"
+    fi
+  done
+}
---
+++ test/pppath.bats
@@ -0,0 +1,141 @@
+#!/usr/bin/env bats
+
+setup () {
+  source "$BATS_TEST_DIRNAME"/../pppath.sh
+}
+
+@test "Show paths." {
+  PATH=/sbin:/bin run pppath
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 2 ]]
+  [[ ${lines[0]} == /sbin ]]
+  [[ ${lines[1]} == /bin ]]
+}
+
+@test "Show multiple paths." {
+  PATH=/sbin:/bin:/usr/sbin:/usr/bin run pppath
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 4 ]]
+  [[ ${lines[0]} == /sbin ]]
+  [[ ${lines[1]} == /bin ]]
+  [[ ${lines[2]} == /usr/sbin ]]
+  [[ ${lines[3]} == /usr/bin ]]
+}
+
+@test "Show multiple paths with raw format." {
+  PATH="$HOME/bin:/sbin:/bin:$HOME/sbin" run pppath --format=raw
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 4 ]]
+  [[ ${lines[0]} == ~/bin ]]
+  [[ ${lines[1]} == /sbin ]]
+  [[ ${lines[2]} == /bin ]]
+  [[ ${lines[3]} == ~/sbin ]]
+}
+
+@test "Show multiple paths with pretty format." {
+  PATH="$HOME/bin:/sbin:/bin:$HOME/sbin" run pppath --format=pretty
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 4 ]]
+  [[ ${lines[0]} == '~/bin' ]]
+  [[ ${lines[1]} == /sbin ]]
+  [[ ${lines[2]} == /bin ]]
+  [[ ${lines[3]} == '~/sbin' ]]
+}
+
+@test "Non existance paths are colored in reversed red." {
+  NO_COLOR=
+  PATH=/foo/bar/sbin:/foo/bar/bin run pppath --color=always
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 2 ]]
+  [[ ${lines[0]} == '[7;31m/foo/bar/sbin[0m' ]]
+  [[ ${lines[1]} == '[7;31m/foo/bar/bin[0m' ]]
+}
+
+@test "Paths under the home directory don't have color." {
+  PATH=~ run pppath --color=always
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 1 ]]
+  [[ ${lines[0]} == '~' ]]
+}
+
+@test "Wellknown or default paths are colored in green." {
+  PATH=/sbin:/bin run pppath --color=always
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 2 ]]
+  [[ ${lines[0]} == '[32m/sbin[0m' ]]
+  [[ ${lines[1]} == '[32m/bin[0m' ]]
+}
+
+@test "Unknown paths are colored in yellow." {
+  PATH=/etc:/usr run pppath --color=always
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 2 ]]
+  [[ ${lines[0]} == '[33m/etc[0m' ]]
+  [[ ${lines[1]} == '[33m/usr[0m' ]]
+}
+
+@test "Duplicate paths are colored in red." {
+  PATH=/bin:/bin run pppath --color=always
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 2 ]]
+  [[ ${lines[0]} == '[31m/bin[0m' ]]
+  [[ ${lines[1]} == '[31m/bin[0m' ]]
+}
+
+@test "Show multiple paths given as arguments." {
+  run pppath /usr/sbin /usr/bin /sbin:/bin
+
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 4 ]]
+  [[ ${lines[0]} == /usr/sbin ]]
+  [[ ${lines[1]} == /usr/bin ]]
+  [[ ${lines[2]} == /sbin ]]
+  [[ ${lines[3]} == /bin ]]
+}
+
+@test "Return an error when invalid options given." {
+  run pppath --color=foo --color=auto
+
+  [[ $status == 1 ]]
+  [[ ${#lines[@]} == 1 ]]
+  [[ ${lines[0]} == "[path] An unknown option: --color=foo." ]]
+
+  run pppath --format=raw --format=bar
+
+  [[ $status == 1 ]]
+  [[ ${#lines[@]} == 1 ]]
+  [[ ${lines[0]} == "[path] An unknown option: --format=bar." ]]
+}
+
+@test "Accept options as arguments even if they are valid after --." {
+  run pppath -- --format=raw --format=bar
+
+  echo "${lines[@]}"
+  [[ $status == 0 ]]
+  [[ ${#lines[@]} == 2 ]]
+  [[ ${lines[0]} == "--format=raw" ]]
+  [[ ${lines[1]} == "--format=bar" ]]
+}
+
+@test "Show help." {
+  run pppath --help
+
+  [[ $status == 0 ]]
+  [[ -n $output ]]
+}
+
+@test "Show version." {
+  run pppath --version
+
+  [[ $status == 0 ]]
+  [[ -n $output ]]
+}
